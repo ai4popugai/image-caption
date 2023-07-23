0d61fad8a75b048128dfbe781f80ef4591f26b8f
ai4popugai <ivan@pheon.com>
2023-07-23 02:31:43 +0200

    remove gpr num classes

--- experiments/EfficientNet_b0/efficient_net_b0.py
+++ experiments/EfficientNet_b0/efficient_net_b0.py
@@ -4,14 +4,12 @@ import torch
 import torchvision.models
 from torch import nn
 
-from datasets.classification.gpr import NUM_CLASSES
-
 
 class EfficientNet(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes: int):
         super().__init__()
         self.base_model = torchvision.models.efficientnet_b0(pretrained=False)
-        self.base_model._fc = nn.Linear(2048, NUM_CLASSES)
+        self.base_model._fc = nn.Linear(2048, num_classes)
 
     def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         x = batch['frames']
