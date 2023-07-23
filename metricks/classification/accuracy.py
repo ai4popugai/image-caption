e37733d46372692a2acb547838618ca34ea96300
ai4popugai <ivan@pheon.com>
2023-07-23 02:12:45 +0200

    a bit accuracy metric refactoring

--- metricks/classification/accuracy.py
+++ metricks/classification/accuracy.py
@@ -2,15 +2,14 @@ from typing import Dict
 
 import torch
 
-from datasets.classification.gpr import NUM_CLASSES
 from metricks.base_metric import BaseMetric
 from torchmetrics import Accuracy as AccuracyMetric
 
 
 class Accuracy(BaseMetric):
-    def __init__(self):
+    def __init__(self, num_classes: int):
         super().__init__(name='accuracy')
-        self.accuracy = AccuracyMetric(task="multiclass", num_classes=NUM_CLASSES)
+        self.accuracy = AccuracyMetric(task="multiclass", num_classes=num_classes)
         self.num = 0
         self.prob_acc = 0.0
 
@@ -22,7 +21,7 @@ class Accuracy(BaseMetric):
         :return: accuracy metric
         """
         self.num += 1
-        self.prob_acc += self.accuracy(result['logits'], batch['labels'])
+        self.prob_acc += self.accuracy(result['logits'], batch['labels']).item()
 
     def compute(self) -> float:
         return self.prob_acc / self.num
