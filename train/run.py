cd86579b3a5180579c85a59276ce29b3a88257d7
ai4popugai <ivan@pheon.com>
2023-07-24 16:59:45 +0200

    add new args and write train method

--- train/run.py
+++ train/run.py
@@ -4,6 +4,8 @@ from typing import Tuple
 
 from torch.utils.data import Dataset
 
+from train.train import Trainer
+
 
 class Run(ABC):
     def __init__(self, filename: str):
@@ -33,7 +35,8 @@ class Run(ABC):
         self.loss = None
 
         # list of metrics
-        self.metrics: None
+        self.train_metrics: None
+        self.val_metrics: None
 
         # num of iterations
         self.train_iters = 100
@@ -48,6 +51,14 @@ class Run(ABC):
         # cudnn
         self.cudnn_benchmark = True
 
+        # augs
+        self.train_augs = None
+        self.val_augs = None
+
+        # metrics
+        self.train_metrics = None
+        self.val_metrics = None
+
     def setup_model(self):
         raise NotImplementedError
 
@@ -56,6 +67,32 @@ class Run(ABC):
 
     def train(self,
               reset_optimizer: bool = False,
-              start_snapshot: str = None,
+              start_snapshot_name: str = None,
               ):
-        pass
+        model = self.setup_model()
+
+        train_dataset, val_dataset = self.setup_datasets()
+
+        trainer = Trainer(batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          train_dataset=train_dataset,
+                          val_dataset=val_dataset,
+                          optimizer=self.optimizer,
+                          loss=self.loss,
+                          snapshot_dir=self.snapshot_dir,
+                          logs_dir=self.logs_dir,
+                          train_metrics=self.train_metrics,
+                          val_metrics=self.val_metrics,
+                          train_augs=self.train_augs,
+                          val_augs=self.val_augs,
+                          train_iters=self.train_iters,
+                          show_iters=self.show_iters,
+                          snapshot_iters=self.snapshot_iters,
+                          )
+        trainer.train(model=model,
+                      reset_optimizer=reset_optimizer,
+                      start_snapshot_name=start_snapshot_name,
+                      max_iteration=self.max_iteration,
+                      lr_policy=self.lr_policy,
+                      strict_weight_loading=self.strict_weight_loading,
+                      )
