56c1fb46e741c2be1653799951db49602196a207
ai4popugai <ivan@pheon.com>
2023-07-24 23:03:43 +0200

    refactor the first phase

--- experiments/EfficientNet_b0/run_0/phase_1.py
+++ experiments/EfficientNet_b0/run_0/phase_1.py
@@ -1,10 +1,14 @@
 from experiments.EfficientNet_b0.run_base import RunBase
+from optim_utils.iter_policy.linear_policy import LinearIterationPolicy
 
 
 class Phase1(RunBase):
     def __init__(self):
         super().__init__(__file__)
 
-        self.optimizer_kwargs = {'lr': 3e-5, 'weight_decay': 3e-7}
-        self.lr_policy = LinearIterationPolicy(0, 0, 5000, 3.0e-4 / 3.2)  # batch size changed from 16 to 5
+        self.optimizer_kwargs = {'weight_decay': 3e-5}
+        self.lr_policy = LinearIterationPolicy(0, 0, 200, 3.0e-4)
 
+
+if __name__ == '__main__':
+    Phase1().train(start_snapshot_name=None)
