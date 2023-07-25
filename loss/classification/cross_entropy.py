bf859c500f4d7584e9801394393c96877788be03
ai4popugai <ivan@pheon.com>
2023-07-25 16:12:50 +0200

    fix loss computing

--- loss/classification/cross_entropy.py
+++ loss/classification/cross_entropy.py
@@ -8,5 +8,5 @@ class CrossEntropyLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, batch: Dict[str, torch.Tensor]) -> torch.Tensor:
-        return nn.CrossEntropyLoss()(batch['logits'], batch['labels'])
+    def forward(self, result: Dict[str, torch.Tensor], batch: Dict[str, torch.Tensor]) -> torch.Tensor:
+        return nn.CrossEntropyLoss()(result['logits'], batch['labels'])
