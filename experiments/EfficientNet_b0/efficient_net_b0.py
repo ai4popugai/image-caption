7213e7e5d189152034d167bb687ee62cbf7f96a5
ai4popugai <ivan@pheon.com>
2023-07-25 16:36:08 +0200

    fix efficientnet model

--- experiments/EfficientNet_b0/efficient_net_b0.py
+++ experiments/EfficientNet_b0/efficient_net_b0.py
@@ -8,10 +8,10 @@ from torch import nn
 class EfficientNet(nn.Module):
     def __init__(self, num_classes: int):
         super().__init__()
-        self.base_model = torchvision.models.efficientnet_b0(pretrained=False)
-        self.base_model._fc = nn.Linear(2048, num_classes)
+        self.model = torchvision.models.efficientnet_b0(pretrained=False)
+        self.model.classifier[1] = nn.Linear(1280, num_classes)
 
     def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         x = batch['frames']
-        x = self.base_model(x)
+        x = self.model(x)
         return {'logits': x}
