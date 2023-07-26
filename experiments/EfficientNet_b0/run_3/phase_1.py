b0eed89f8bd27194267f2c6f5d38b417459cf271
ai4popugai <ivan@pheon.com>
2023-07-26 06:12:46 +0200

    fix all runs

--- experiments/EfficientNet_b0/run_3/phase_1.py
+++ experiments/EfficientNet_b0/run_3/phase_1.py
@@ -1,10 +1,12 @@
+import os
+
 from experiments.EfficientNet_b0.run_base import RunBase
 from optim_utils.iter_policy.linear_policy import LinearIterationPolicy
 
 
 class Phase1(RunBase):
     def __init__(self):
-        super().__init__(__file__)
+        super().__init__(os.path.abspath(__file__))
 
         self.optimizer_kwargs = {'weight_decay': 3e-5}
         self.lr_policy = LinearIterationPolicy(0, 0, 3000, 6e-4)
