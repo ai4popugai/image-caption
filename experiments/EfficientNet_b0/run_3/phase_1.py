6ed822c3b9bfbe14f95be8d2e7bef58a6e13afcc
ai4popugai <ivan@pheon.com>
2023-07-26 06:00:57 +0200

    add new run for long training

---
+++ experiments/EfficientNet_b0/run_3/phase_1.py
@@ -0,0 +1,16 @@
+from experiments.EfficientNet_b0.run_base import RunBase
+from optim_utils.iter_policy.linear_policy import LinearIterationPolicy
+
+
+class Phase1(RunBase):
+    def __init__(self):
+        super().__init__(__file__)
+
+        self.optimizer_kwargs = {'weight_decay': 3e-5}
+        self.lr_policy = LinearIterationPolicy(0, 0, 1500, 6e-4)
+
+        self.max_iteration = 16500
+
+
+if __name__ == '__main__':
+    Phase1().train(start_snapshot_name=None)
