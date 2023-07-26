f3ce5463c8c58912c7b8dd920a3c84192b2800d5
ai4popugai <ivan@pheon.com>
2023-07-26 14:57:40 +0200

    add rotate aug

--- augmentations/classification/augs.py
+++ augmentations/classification/augs.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod, ABC
 from typing import Dict, Tuple
 
+import cv2
+import numpy as np
 import torch
 from torch import nn
 from torchvision import transforms
@@ -57,3 +59,40 @@ class CenterCrop(BaseAug):
     def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         transformed_frames = transforms.CenterCrop(self.size)(batch['frames'])
         return {'frames': transformed_frames, 'labels': batch['labels']}
+
+
+class Rotate(BaseAug):
+    def __init__(self, angle_range=(-180, 180)):
+        super().__init__()
+        self.angle_range = angle_range
+
+    def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        transformed_frames = batch['frames']
+
+        # Randomly select an angle within the defined range
+        angle = torch.FloatTensor(1).uniform_(self.angle_range[0], self.angle_range[1]).item()
+
+        # Perform rotation on each frame
+        for i in range(transformed_frames.shape[0]):
+            transformed_frames[i] = self.rotate_frame(transformed_frames[i], angle)
+
+        return {'frames': transformed_frames, 'labels': batch['labels']}
+
+    @staticmethod
+    def rotate_frame(frame, angle):
+        # Convert frame to numpy array
+        frame = frame.numpy()
+
+        height, width = frame.shape[:2]
+        center = (width / 2, height / 2)
+
+        # Get the rotation matrix
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+
+        # Perform the actual rotation
+        rotated_frame = cv2.warpAffine(frame, rotation_matrix, (width, height))
+
+        # Convert back to torch.Tensor
+        rotated_frame = torch.from_numpy(rotated_frame)
+
+        return rotated_frame
