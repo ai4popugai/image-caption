5b92095a833a9270eb1135003bcca4c38eeb1c1d
ai4popugai <ivan@pheon.com>
2023-07-26 15:03:32 +0200

    add new run

---
+++ experiments/EfficientNet_b0/run_8/phase_1.py
@@ -0,0 +1,19 @@
+import os
+
+from augmentations.classification.augs import RandomFlip, RandomCrop, Rotate
+from experiments.EfficientNet_b0.run_base import RunBase
+
+
+class Phase1(RunBase):
+    def __init__(self):
+        super().__init__(os.path.abspath(__file__))
+
+        self.optimizer_kwargs = {'lr': 2.4e-3, 'weight_decay': 3e-5}
+        self.lr_policy = None
+
+        self.train_augs = [RandomFlip(), Rotate(), RandomCrop(self.crop_size)]
+        self.num_workers = 4
+
+
+if __name__ == '__main__':
+    Phase1().train(start_snapshot_name=None)
