631b633240b80ae1ca4ea68c078db682640264f1
ai4popugai <ivan@pheon.com>
2023-07-27 16:17:43 +0200

    improve new script

--- datasets/concept_detection/utils/cd_from_gpr.py
+++ datasets/concept_detection/utils/cd_from_gpr.py
@@ -1,25 +1,29 @@
 import os
+import shutil
 
 import torch
 from torch.utils.data import DataLoader
 
 from datasets.classification.gpr import GPRDataset
 from experiments.EfficientNet_b0.run_16.phase_1 import Phase1
+from models.classification.feature_extractor import FeatureExtractor
 from train.train import Trainer
 
 
-def create_dataset(snapshot_name: str, batch_size: int = 64, num_workers: int = 8):
+def create_dataset(snapshot_name: str, dst_dir: str):
     """
     Function created new dataset from existing one.
     All images from GPR dataset pass through Efficient without last layer and stores in new dataset.
-    :param batch_size:
-    :param num_workers: number of workers for dataloader
+    :param dst_dir: directory where new dataset will be stored
     :param snapshot_name: name of snapshot from which we take model
     :return:
     """
+    emd_dir = os.path.join(dst_dir, 'emd')
+    os.makedirs(emd_dir, exist_ok=True)
+
     # init run and model
     run = Phase1()
-    model = run.setup_model()
+    model = FeatureExtractor(run.setup_model())
 
     # load snapshot
     snapshot_path = os.path.join(run.snapshot_dir, snapshot_name)
@@ -27,10 +31,9 @@ def create_dataset(snapshot_name: str, batch_size: int = 64, num_workers: int = 
 
     # datasets and loaders
     dataset = GPRDataset(resolution=run.resolution)
-    dataloader = DataLoader(dataset, batch_size=batch_size,
-                            shuffle=False, num_workers=num_workers)
-    iterator = iter(dataloader)
-    iters = len(dataloader)
+
+    # copy description
+    shutil.copyfile(dataset.description_path, dst_dir)
 
     # setup device
     device = Trainer.get_device()
@@ -39,15 +42,19 @@ def create_dataset(snapshot_name: str, batch_size: int = 64, num_workers: int = 
     model.to(device)
     model.eval()
     with torch.no_grad():
-        for _ in range(iters):
-            batch = next(iterator)
+        for ff in dataset.frames_list:
+            # get id to save
+            id = os.path.splitext(os.path.basename(ff))[0]
+
+            # get batch
+            ff_idx = dataset.frames_list.index(ff)
+            batch = dataset[ff_idx]
+            batch['frames'] = batch['frames'].unsqueeze(0)
+
+            # prepare batch
             batch = Trainer.batch_to_device(batch, device)
             batch = Trainer.aug_loop(run.val_augs, batch)
             batch = Trainer.normalize(batch, run.normalizer)
 
-
-
-
-
-
-
+            # inference
+            result = model(batch)
