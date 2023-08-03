49a50aa339ab5560469caef7622a69845caa1d49
ai4popugai <ivan@pheon.com>
2023-08-03 19:58:04 +0200

    add feature map extractor for efficient net

--- experiments/EfficientNet_b0/efficient_net_b0.py
+++ experiments/EfficientNet_b0/efficient_net_b0.py
@@ -12,3 +12,14 @@ class EfficientNet(BaseClassificationModel):
         super().__init__()
         self.model = torchvision.models.efficientnet_b0()
         self.model.classifier[1] = nn.Linear(1280, num_classes)
+
+
+class EfficientNetFeatureMapExtractor(BaseClassificationModel):
+    def __init__(self, model: EfficientNet, num_leave_layers: int = 4):
+        super().__init__()
+        self.model = nn.Sequential(*list(model.get_model().features)[:-num_leave_layers])
+
+    def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        x = batch['frames']
+        x = self.model(x)
+        return {'feature_maps': x}
