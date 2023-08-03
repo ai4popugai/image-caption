7f6f6532732ea1c28cd9ff67f15171cd471d716f
ai4popugai <ivan@pheon.com>
2023-08-03 20:14:10 +0200

    refactor concept detection dataset getting

--- datasets/concept_detection/utils/concept_detection_from_gpr.py
+++ datasets/concept_detection/utils/concept_detection_from_gpr.py
@@ -6,15 +6,16 @@ import torch
 from torch.utils.data import DataLoader
 
 from datasets.classification.gpr import GPRDataset
-from models.classification.feature_extractor import FeatureExtractor
+from experiments.EfficientNet_b0.efficient_net_b0 import EfficientNetFeatureMapExtractor
 from train.train import Trainer
 
 
-def create_dataset(experiment: str, run: str, phase: str, snapshot_name: str, dst_dir: str, ):
+def create_dataset(experiment: str, run: str, phase: str, snapshot_name: str, dataset_dir: str, name_prefix: str = ''):
     """
     Function creates a new dataset from an existing one.
     All images from GPR dataset pass through EfficientNet without the last layer and are stored in the new dataset.
-    :param dst_dir: directory where the new dataset will be stored
+    :param name_prefix: prefix for the name of the new dataset. If empty, the name of the snapshot is used.
+    :param dataset_dir: directory where the new dataset will be stored
     :param snapshot_name: name of the snapshot from which we take the model
     :param experiment: name of the experiment (e.g., "EfficientNet_b0")
     :param run: name of the run (e.g., "run_16")
@@ -25,7 +26,8 @@ def create_dataset(experiment: str, run: str, phase: str, snapshot_name: str, ds
     device = Trainer.get_device()
     # device = torch.device('cpu')
 
-    emd_dir = os.path.join(dst_dir, 'embs')
+    dst_dir = os.path.join(dataset_dir, f'{name_prefix}_{run}_{phase}_{os.path.splitext(snapshot_name)[0]}')
+    emd_dir = os.path.join(dst_dir, 'feature_maps')
     os.makedirs(emd_dir, exist_ok=True)
 
     # Convert experiment, run, and phase to module paths
@@ -41,7 +43,7 @@ def create_dataset(experiment: str, run: str, phase: str, snapshot_name: str, ds
     model.load_state_dict(checkpoint['model_state_dict'], strict=True)
 
     # create feature extractor
-    model = FeatureExtractor(model)
+    model = EfficientNetFeatureMapExtractor(model)
 
     # datasets and loaders
     dataset = GPRDataset(resolution=run_instance.resolution)
@@ -69,7 +71,7 @@ def create_dataset(experiment: str, run: str, phase: str, snapshot_name: str, ds
 
             # inference
             result = model(batch)
-            torch.save(result['embs'].squeeze(), os.path.join(emd_dir, f'{id}.pth'))
+            torch.save(result['feature_maps'].squeeze(), os.path.join(emd_dir, f'{id}.pth'))
 
 
 if __name__ == "__main__":
@@ -78,7 +80,8 @@ if __name__ == "__main__":
     parser.add_argument("--run", type=str, help="Name of the run (e.g., 'run_16')")
     parser.add_argument("--phase", type=str, help="Name of the phase (e.g., 'phase_1')")
     parser.add_argument("--snapshot_name", type=str, help="Name of the snapshot from which to take the model")
-    parser.add_argument("--dst_dir", type=str, help="Directory where the new dataset will be stored")
+    parser.add_argument("--dataset_dir", type=str, help="Directory where the new dataset will be stored")
+    parser.add_argument("--name_prefix", type=str, help='Prefix for the name of the new dataset')
     args = parser.parse_args()
 
-    create_dataset(args.experiment, args.run, args.phase, args.snapshot_name, args.dst_dir, )
+    create_dataset(args.experiment, args.run, args.phase, args.snapshot_name, args.dataset_dir, args.name_prefix,)
