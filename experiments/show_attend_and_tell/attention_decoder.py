8533827313827d36b7393a33d0b2c6838bc7a8df
ai4popugai <ivan@pheon.com>
2023-08-03 23:21:38 +0200

    add private forward method

--- experiments/show_attend_and_tell/attention_decoder.py
+++ experiments/show_attend_and_tell/attention_decoder.py
@@ -1,10 +1,12 @@
+import torch
 from torch import nn
 
 from models.functional import BahdanauAttention
 
 
 class AttentionDecoder(nn.Module):
-    def __init__(self, vocab_size: int, embedding_size: int, hidden_size: int, keys_hidden_size: int):
+    def __init__(self, vocab_size: int, embedding_size: int, hidden_size: int, keys_hidden_size: int, num_layers=4,
+                 max_len=15):
         super(AttentionDecoder, self).__init__()
         # attention mechanism for feature maps and hidden state
         self.attention = BahdanauAttention(hidden_size, keys_hidden_size)
@@ -16,6 +18,30 @@ class AttentionDecoder(nn.Module):
         # embedding layer to convert words indexex to embeddings
         self.embedding = nn.Embedding(vocab_size, embedding_size)
         # rnn cell
-        self.rnn = nn.GRU(embedding_size + hidden_size, hidden_size)
+        self.rnn = nn.GRU(embedding_size + hidden_size, hidden_size, num_layers=num_layers, batch_first=True)
         # linear layer to make final predictions
         self.fc_1 = nn.Linear(hidden_size, vocab_size)
+        self.max_len = max_len
+
+    def _forward(self, inputs, hidden, keys):
+        # inputs: (batch_size, 1)
+        # hidden: (batch_size, hidden_size)
+        # keys: (batch_size, seq_len, keys_hidden_size)
+        # embedding: (batch_size, 1, embedding_size)
+        embedding = self.embedding(inputs)
+        # context: (batch_size, 1, keys_hidden_size)
+        # weights: (batch_size, 1, seq_len)
+        context, weights = self.attention(hidden.unsqueeze(1), keys)
+        # context: (batch_size, 1, hidden_size)
+        context = self.fc_0(context)
+        # rnn_input: (batch_size, 1, embedding_size + hidden_size)
+        rnn_input = torch.cat([embedding, context], dim=-1)
+        # output: (batch_size, 1, hidden_size)
+        # hidden: (batch_size, hidden_size)
+        # GRU takes input of shape (batch_size, seq_len, input_size) and hidden of shape (1, batch_size, hidden_size)
+        output, hidden = self.rnn(rnn_input, hidden.unsqueeze(0))
+        # output: (batch_size, hidden_size)
+        output = output.squeeze(1)
+        # output: (batch_size, vocab_size)
+        output = self.fc_1(output)
+        return output, hidden.squeeze(0), weights
