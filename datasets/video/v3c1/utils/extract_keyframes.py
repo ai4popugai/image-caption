a6efa7c37c108609c2dc59026d50fe147ae91d69
ai4popugai <ivan@pheon.com>
2023-08-08 16:35:07 +0200

    add n_per_scene variable

--- datasets/video/v3c1/utils/extract_keyframes.py
+++ datasets/video/v3c1/utils/extract_keyframes.py
@@ -10,7 +10,7 @@ from utils.video_utils.video_reader import VideoReader
 FPS = 25
 
 
-def extract_keyframes(dataset_path: str):
+def extract_keyframes(dataset_path: str, n_frames: int):
     d_dirname, d_name = os.path.split(dataset_path)
     keyframes_path = os.path.join(d_dirname, f'{d_name}_keyframes')
     part_dirs_list = [part_dir for part_dir in sorted(os.listdir(dataset_path))]
@@ -22,19 +22,20 @@ def extract_keyframes(dataset_path: str):
             os.makedirs(dst_dir, exist_ok=True)
 
             scene_list = detect(video_path, ContentDetector())
+            scene_list = scene_list[1:-1]
+
+            n_per_scene = n_frames // len(scene_list) + 1
             reader = VideoReader(video_path, fps=FPS)
 
-            for scene in scene_list[1:-1]:
+            keyframes_id_list = []
+            for scene in scene_list:
                 scene_start_idx = scene[0].frame_num
                 scene_end_idx = scene[1].frame_num
-                scene_len = scene_end_idx - scene_start_idx
-                scene_middle_idx = (scene_start_idx + scene_end_idx) // 2
-
-                # get idx from 1/4 to 3/4 of scene
-                keyframe_id = random.randint(scene_middle_idx - scene_len // 4,
-                                             scene_middle_idx + scene_len // 4)
-                keyframe = reader[keyframe_id]
-                cv2.imwrite(os.path.join(dst_dir, f'frame_{"%05d" % keyframe_id}.png'), keyframe)
+                for _ in range(n_per_scene):
+                    keyframe_id = random.randint(scene_start_idx, scene_end_idx)
+                    keyframes_id_list.append(keyframe_id)
+                # keyframe = reader[keyframe_id]
+                # cv2.imwrite(os.path.join(dst_dir, f'frame_{"%05d" % keyframe_id}.png'), keyframe)
 
             print(f'{video_path} video is ready.')
 
