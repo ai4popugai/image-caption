6ee86d37689ff15678bfae7960afd24580ab42a2
ai4popugai <ivan@pheon.com>
2023-08-21 21:23:58 +0200

    add 2 utilities function

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,21 +1,36 @@
 import os
+from abc import ABCMeta
 
 import torch
 
+from experiments.EfficientNet_b0.run_base import RunBase
 
-def setup_model(experiment: str, run: str, phase: str, snapshot_name: str):
+
+def setup_run_instance(experiment: str, run: str, phase: str,) -> RunBase:
     """
-    Script set up model by experiment, run, phase and snapshot.
+    Setup run instance
 
     :param experiment: name of the experiment (e.g., "EfficientNet_b0")
     :param run: name of the run (e.g., "run_16")
     :param phase: name of the phase (e.g., "phase_1")
-    :param snapshot_name: name of the snapshot from which we take the model
-    :return: model with loaded weights.
+    :return: run instance
     """
+    # Convert experiment, run, and phase to module paths
     run_module = __import__(f'experiments.{experiment}.{run}', fromlist=[phase])
     phase_module = getattr(run_module, phase)
     run_instance = phase_module.Phase1()
+
+    return run_instance
+
+
+def setup_pretrained_model(run_instance: RunBase, snapshot_name: str) -> torch.nn.Module:
+    """
+    Script set up model by experiment, run, phase and snapshot.
+
+    :param run_instance: RunBase instance class
+    :param snapshot_name: name of the snapshot from which we take the model
+    :return: model with loaded weights.
+    """
 
     model = run_instance.setup_model()
 
