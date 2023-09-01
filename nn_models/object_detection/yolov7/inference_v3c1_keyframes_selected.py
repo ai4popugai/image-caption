4bc37fefe83d43d85dcc2055d7a18be1da134e0b
ai4popugai <ivan@pheon.com>
2023-09-01 20:17:23 +0200

    add script to add objects to v3c1_keyframes_selected

--- nn_models/object_detection/yolov7/inference_v3c1_keyframes_selected.py
+++ nn_models/object_detection/yolov7/inference_v3c1_keyframes_selected.py
@@ -1,11 +1,31 @@
+import argparse
+import glob
+import json
+import os
+
 from yolov7_package import Yolov7Detector
 import cv2
 
-if __name__ == '__main__':
-    img = cv2.imread('/Users/iivan/datasets/v3c1_keyframes_selected/00001.mp4/frame_05431.png')
-    det = Yolov7Detector(traced=False)
-    classes, boxes, scores = det.detect(img)
-    img = det.draw_on_image(img, boxes[0], scores[0], classes[0])
 
-    cv2.imshow("image", img)
-    cv2.waitKey()
+def inference(v3c1_keyframes_dir: str, threshold: float):
+    det = Yolov7Detector(traced=False)
+    img_folder_list = [os.path.join(v3c1_keyframes_dir, img_folder) for img_folder in os.listdir(v3c1_keyframes_dir)]
+    for img_folder in img_folder_list:
+        img_path = glob.glob(f'{img_folder}/*.png')[0]
+        img = cv2.imread(img_path)
+        classes, boxes, scores = det.detect(img)
+        objects = []
+        for class_id, box, score in zip(classes[0], boxes[0], scores[0]):
+            if score >= threshold:
+                objects.append({det.names[class_id]: box})
+                store_path = os.path.join(img_folder, 'objects.json')
+                with open(store_path, 'w') as f:
+                    json.dump(objects, f)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Object detection script')
+    parser.add_argument('--v3c1_keyframes_dir', required=True, help='Path to the directory containing keyframes')
+    parser.add_argument('--threshold', type=float, default=0.5, help='Detection threshold')
+    args = parser.parse_args()
+    inference(args.v3c1_keyframes_dir, args.threshold)
