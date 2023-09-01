dda06d9a9bff0746ace291a48b72da56d72713cb
ai4popugai <ivan@pheon.com>
2023-09-01 20:30:18 +0200

    store also image with drawn objects

--- nn_models/object_detection/yolov7/inference_v3c1_keyframes_selected.py
+++ nn_models/object_detection/yolov7/inference_v3c1_keyframes_selected.py
@@ -9,7 +9,8 @@ import cv2
 
 def inference(v3c1_keyframes_dir: str, threshold: float):
     det = Yolov7Detector(traced=False)
-    img_folder_list = [os.path.join(v3c1_keyframes_dir, img_folder) for img_folder in os.listdir(v3c1_keyframes_dir)]
+    img_folder_list = [os.path.join(v3c1_keyframes_dir, img_folder)
+                       for img_folder in sorted(os.listdir(v3c1_keyframes_dir))]
     for img_folder in img_folder_list:
         img_path = glob.glob(f'{img_folder}/*.png')[0]
         img = cv2.imread(img_path)
@@ -18,11 +19,13 @@ def inference(v3c1_keyframes_dir: str, threshold: float):
         for class_id, box, score in zip(classes[0], boxes[0], scores[0]):
             if score >= threshold:
                 objects.append({det.names[class_id]: box})
+                img = det.draw_on_image(img, [box], [score], [class_id])
         store_path = os.path.join(img_folder, 'objects.json')
         if os.path.isfile(store_path):
             os.remove(store_path)
         with open(store_path, 'w') as f:
             json.dump(objects, f)
+        cv2.imwrite(os.path.join(img_folder, 'objects.png'), img)
 
 
 if __name__ == '__main__':
