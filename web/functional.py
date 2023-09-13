656792c5f6dac40ffc918001b6300cba72dfd12d
ai4popugai <ivan@pheon.com>
2023-09-13 13:29:27 +0200

    little adjustment

--- web/functional.py
+++ web/functional.py
@@ -2,23 +2,18 @@ import os
 import random
 
 from datasets.video.v3c1.utils.extract_keyframes import extract_keyframes
-
-KEYFRAMES_KEY = 'keyframes'
+from web import KEYFRAMES_DIR_KEY
 
 
 def main(videos_dir: str, n_frames: int,):
     random.seed(0)
-
-    # create directory with keyframes
-    keyframes_dir = os.path.join(videos_dir, KEYFRAMES_KEY)
-    os.makedirs(keyframes_dir)
 
     videos_list = [video for video in sorted(os.listdir(videos_dir)) if video.endswith('.mp4')]
     # process videos one by one
     for video_name in videos_list:
         # setup paths
         video_path = os.path.join(videos_dir, video_name)
-        keyframes_path = os.path.join(videos_dir, KEYFRAMES_KEY, video_name)
+        keyframes_path = os.path.join(videos_dir, KEYFRAMES_DIR_KEY, video_name)
         os.makedirs(keyframes_path, exist_ok=True)
 
         # extract keyframes
