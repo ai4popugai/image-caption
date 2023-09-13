541711dcbf80007886073502ce7def81c218d0f1
ai4popugai <ivan@pheon.com>
2023-09-13 20:53:22 +0200

    add method to add concept to row

--- db/db.py
+++ db/db.py
@@ -30,3 +30,10 @@ class SQLiteDb:
             cursor.execute(f"INSERT INTO {self.db_name}({VIDEO_ID_KEY},{KEYFRAME_ID_KEY}) "
                            f"VALUES ('{video_id}', '{keyframe_id}')")
             connect.commit()
+
+    def add_concept_to_row(self, video_id, keyframe_id, concept):
+        with sqlite3.connect(self.db_path) as connect:
+            cursor = connect.cursor()
+            cursor.execute(f"UPDATE {self.db_name} SET {CONCEPT_KEY} = '{concept}' "
+                           f"WHERE {VIDEO_ID_KEY} = '{video_id}' AND {KEYFRAME_ID_KEY} = '{keyframe_id}'")
+            connect.commit()
