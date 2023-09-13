5bca86eb5b687a1c73c7e6545dc7da8f505438fb
ai4popugai <ivan@pheon.com>
2023-09-13 16:32:49 +0200

    little refactoring

--- db/db.py
+++ db/db.py
@@ -11,17 +11,17 @@ class SQLiteDb:
 
         self.version = len(glob.glob(os.path.join(self.db_dir, f"{db_name}*")))
         self.db_name = f"{db_name}_{self.version}"
-        self._db_path = os.path.join(self.db_dir, self.db_name)
+        self.db_path = os.path.join(self.db_dir, self.db_name)
         self._create_db()
 
     def _create_db(self):
-        with sqlite3.connect(self._db_path) as connect:
+        with sqlite3.connect(self.db_path) as connect:
             cursor = connect.cursor()
             cursor.execute(f"CREATE TABLE {self.db_name}({VIDEO_ID_KEY},{KEYFRAME_ID_KEY},"
                            f"{OBJECTS_KEY},{CONCEPT_KEY})")
 
     def add_new_key(self, video_id, keyframe_id):
-        with sqlite3.connect(self._db_path) as connect:
+        with sqlite3.connect(self.db_path) as connect:
             cursor = connect.cursor()
             cursor.execute(f"INSERT INTO {self.db_name}({VIDEO_ID_KEY},{KEYFRAME_ID_KEY}) "
                            f"VALUES ('{video_id}', '{keyframe_id}')")
