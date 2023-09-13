2fde8563f3f7f489413080826d3c46fc553dc300
ai4popugai <ivan@pheon.com>
2023-09-13 15:59:46 +0200

    fix crearing db

--- db/db.py
+++ db/db.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import sqlite3
 
@@ -7,8 +8,8 @@ from db import VIDEO_ID_KEY, KEYFRAME_ID_KEY, OBJECTS_KEY, CONCEPT_KEY
 class SQLiteDb:
     def __init__(self, db_name):
         self.db_dir = os.environ["SQLITE_DB_DIR"]
-        self.version = 0 if os.path.isfile(os.path.join(self.db_dir, db_name)) is False \
-            else int(os.path.join(self.db_dir, db_name).split('_')[-1]) + 1
+
+        self.version = len(glob.glob(os.path.join(self.db_dir, f"{db_name}*")))
         self.db_name = f"{db_name}_{self.version}"
         self._db_path = os.path.join(self.db_dir, self.db_name)
         self._create_db()
@@ -16,5 +17,5 @@ class SQLiteDb:
     def _create_db(self):
         with sqlite3.connect(self._db_path) as connect:
             cursor = connect.cursor()
-            cursor.execute(f"CREATE TABLE {self._db_path}({VIDEO_ID_KEY},{KEYFRAME_ID_KEY},"
+            cursor.execute(f"CREATE TABLE {self.db_name}({VIDEO_ID_KEY},{KEYFRAME_ID_KEY},"
                            f"{OBJECTS_KEY},{CONCEPT_KEY})")
