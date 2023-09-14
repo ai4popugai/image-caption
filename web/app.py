92ecf306c2a5a0a1adb591e4a4c9413ce9fc3e5b
ai4popugai <ivan@pheon.com>
2023-09-14 14:40:31 +0200

    add processing video after uploading

--- web/app.py
+++ web/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template, flash, redirect, url_for
 import os
 import threading
 
+from web.web_functional import preprocess_videos
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
@@ -30,7 +32,17 @@ def upload_videos():
 
     flash(f'{len(files)} video(s) uploaded successfully.')
 
-    return "Videos uploaded successfully."
+    # Redirect the user to the process_videos route
+    return redirect(url_for('process_uploaded_videos'))
+
+
+@app.route('/process_videos')
+def process_uploaded_videos():
+    # Apply your preprocess_videos method to the UPLOAD folder
+    preprocessing_thread = threading.Thread(target=preprocess_videos, args=(app.config['UPLOAD_FOLDER'], 50))
+    preprocessing_thread.start()
+
+    return "Video processing started."
 
 
 if __name__ == '__main__':
