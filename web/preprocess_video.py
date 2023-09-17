451d2fd73066798539c5ac25d7d82da3c1003434
ai4popugai <ivan@pheon.com>
2023-09-17 15:23:36 +0200

    fix db argument bug

--- web/preprocess_video.py
+++ web/preprocess_video.py
@@ -45,4 +45,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     database = SQLiteDb(os.path.join(os.environ['SQLITE_DB_DIR'], os.path.basename(args.videos_dir)))
-    preprocess_videos(args.videos_dir, args.n_frames)
+    preprocess_videos(args.videos_dir, args.n_frames, database)
