ba71bc391997d0a40646383e905e62028bb21325
ai4popugai <ivan@pheon.com>
2023-09-18 14:04:20 +0200

    refactor _forward and _forward_inference

--- nn_models/image_description/show_attend_and_tell/attention_decoder.py
+++ nn_models/image_description/show_attend_and_tell/attention_decoder.py
@@ -10,7 +10,7 @@ from nn_models.functional import BahdanauAttention
 class AttentionDecoder(nn.Module):
     def __init__(self, vocab_size: int, embedding_size: int, hidden_size: int, keys_hidden_size: int,
                  sos_tokenized: torch.Tensor, eos_tokenized: torch.Tensor,
-                 num_layers=4, max_len=15,):
+                 num_layers=4, max_len=15, ):
         super().__init__()
         # attention mechanism for feature maps and hidden state
         self.attention = BahdanauAttention(hidden_size, keys_hidden_size)
@@ -32,7 +32,7 @@ class AttentionDecoder(nn.Module):
         self.sos_tokenized = sos_tokenized
         self.eos_tokenized = eos_tokenized
 
-    def _tokens_to_x(self, tokenized_words: torch.Tensor) -> torch.Tensor:
+    def _tokens_to_hidden(self, tokenized_words: torch.Tensor) -> torch.Tensor:
         """
         Convert tokenized words firstly to embeddings and then to x.
 
@@ -44,32 +44,29 @@ class AttentionDecoder(nn.Module):
         return self.fc_1(embeddings)  # x: (batch_size, seq_len, hidden_size)
         # or (batch_size, hidden_size) - ready to go captions
 
-    def _forward(self, x_t: torch.Tensor, hidden: torch.Tensor, keys: torch.Tensor) \
+    def _forward(self, s_hidden: torch.Tensor, keys: torch.Tensor) \
             -> (torch.Tensor, torch.Tensor, torch.Tensor):
         """
-        One step forward of the decoder, given the input x_t, the hidden state and the keys (feature maps).
-        x_t is the word ready to go vector of the previous gt word if training mode or previous predicted word
-        if inference mode,
-        hidden is the hidden state of the previous time step.
+        One step forward of the decoder, given the hidden state s_hidden and the keys (feature maps).
+        s_hidden is the hidden state of the previous time step.
 
-        :param x_t: ready to go vector of the previous gt word or previous predicted word (batch_size, hidden_size).
-        :param hidden: hidden state of the previous time step (num_layers, batch_size, hidden_size).
+        :param s_hidden: hidden state of the previous time step (num_layers, batch_size, hidden_size).
         :param keys: feature maps from the encoder (batch_size, seq_len, keys_hidden_size).
         :return: decoded word (batch_size, vocab_size), hidden state (batch_size, hidden_size) and attention weights.
         """
         # I get hidden state (batch_size, 1, hidden_size) from the last layer of GRU to the attention layer
-        context, weights = self.attention(hidden[-1].unsqueeze(1), keys)  # context: (batch_size, 1, keys_hidden_size)
-                                                                           # weights: (batch_size, 1, seq_len)
+        context, weights = self.attention(s_hidden[-1].unsqueeze(1), keys)  # context: (batch_size, 1, keys_hidden_size)
+        # weights: (batch_size, 1, seq_len)
         context = self.fc_0(context)  # context: (batch_size, 1, hidden_size)
         # rnn_input: (batch_size, 1, hidden_size + hidden_size)
-        rnn_input = torch.cat([x_t.unsqueeze(1), context], dim=-1)
+        rnn_input = torch.cat([s_hidden[-1].unsqueeze(1), context], dim=-1)
         # GRU takes input of shape (batch_size, 1, 2 * hidden_size
         # and hidden of shape (num_layers, batch_size, hidden_size)
-        output, hidden = self.rnn(rnn_input, hidden)   # output: (batch_size, 1, hidden_size)
-                                                       # hidden: (num_layers, batch_size, hidden_size)
-        output = output.squeeze(1)  # output: (batch_size, hidden_size)
-        output = self.fc_2(output)  # output: (batch_size, vocab_size)
-        return output, hidden, weights
+        y_t, s = self.rnn(rnn_input, s_hidden)  # output: (batch_size, 1, hidden_size)
+        # hidden: (num_layers, batch_size, hidden_size)
+        y_t = y_t.squeeze(1)  # output: (batch_size, hidden_size)
+        y_t = self.fc_2(y_t)  # output: (batch_size, vocab_size)
+        return y_t, s, weights
 
     def _forward_inference(self, features: torch.Tensor) -> torch.Tensor:
         """
@@ -82,25 +79,22 @@ class AttentionDecoder(nn.Module):
 
         batch_size = features.shape[0]
         assert batch_size == 1, "In inference mode batch size must be 1."
-        # hidden: (num_layers, batch_size, hidden_size)
-        hidden = torch.zeros(self.num_layers, batch_size, self.hidden_size, device=features.device)
+        # s_hidden: (num_layers, batch_size, hidden_size)
+        s_hidden = self._tokens_to_hidden(self.sos_tokenized)\
+            .repeat(batch_size).repeat(self.num_layers).to(features.device)
         # outputs: (batch_size, max_len, vocab_size)
         outputs = torch.zeros(batch_size, self.max_len, self.vocab_size, device=features.device)
 
-        # initialize the input with SOS token
-        x_t = self._tokens_to_x(self.sos_tokenized).repeat(batch_size)  # x_t: (batch_size, hidden_size)
-
         for t in range(self.max_len):
-            output, hidden, _ = self._forward(x_t, hidden, features)  # output: (batch_size, vocab_size)
-                                                                      # hidden: (num_layers, batch_size, hidden_size)
-            outputs[:, t, :] = output
+            y_t, s_hidden, _ = self._forward(s_hidden, features)  # output: (batch_size, vocab_size)
+                                                                  # s_hidden: (num_layers, batch_size, hidden_size)
+            outputs[:, t, :] = y_t
             # get the most probable word index
-            decoded_token = output.argmax(dim=-1)  # decoded_token: (batch_size,)
+            decoded_token = y_t.argmax(dim=-1)  # decoded_token: (batch_size,)
             # check if decoded_token is EOS token
             if (decoded_token == self.eos_tokenized).all().item():
                 return outputs[:, :t + 1, :]
-            # get the ready to go vector of the predicted word
-            x_t = self._tokens_to_x(decoded_token)  # x_t: (batch_size, hidden_size)
+
         return outputs
 
     def _forward_training(self, features: torch.Tensor, captions: torch.Tensor) -> torch.Tensor:
@@ -112,7 +106,7 @@ class AttentionDecoder(nn.Module):
         :return: (batch_size, seq_len - 1, vocab_size) - predictions for each token in the sequence except EOS token.
         """
         out_seq_len = captions.shape[1] - 1  # -1 because we don't need to predict SOS token
-        x = self._tokens_to_x(captions)  # x: (batch_size, seq_len, hidden_size) - ready to go captions' vectors
+        x = self._tokens_to_hidden(captions)  # x: (batch_size, seq_len, hidden_size) - ready to go captions' vectors
         batch_size = features.shape[0]
         # hidden: (num_layers, batch_size, hidden_size)
         hidden = torch.zeros(self.num_layers, batch_size, self.hidden_size, device=features.device)
@@ -141,4 +135,3 @@ class AttentionDecoder(nn.Module):
             outputs = self._forward_inference(features)  # (batch_size, any_seq_len <= self.max_len, vocab_size)
 
         return {LOGITS_KEY: outputs}
-
