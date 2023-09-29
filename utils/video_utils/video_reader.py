58d68ead46d85bbdad8da0b677b300bf7ca6e937
ai4popugai <ivan@pheon.com>
2023-09-29 18:52:07 +0200

    remove print, fix grammar

--- utils/video_utils/video_reader.py
+++ utils/video_utils/video_reader.py
@@ -10,7 +10,6 @@ class VideoReader:
     def __init__(self, video_path: str, scale_factor: float = 1.,):
         super().__init__()
         self.cap = cv2.VideoCapture(video_path)
-        print(self.cap.get(cv2.CAP_PROP_FPS))
         self.height, self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * scale_factor), \
                                   int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH) * scale_factor)
         self.frames_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -35,7 +34,7 @@ class VideoReader:
 
     def __getitem__(self, frame_idx):
         if frame_idx >= self.frames_count:
-            raise IndexError(f'Max index is {self.frames_count - 1}, you get {frame_idx}')
+            raise IndexError(f'Max index is {self.frames_count - 1}, your index: {frame_idx}')
         self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
         _, frame = self.cap.read()
         return frame
