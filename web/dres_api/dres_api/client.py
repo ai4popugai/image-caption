6082a6365757f627b4a81dcb2064090895fd85a4
ai4popugai <ivan@pheon.com>
2023-10-07 15:03:26 +0200

    refactor dres client

--- web/dres_api/dres_api/client.py
+++ web/dres_api/dres_api/client.py
@@ -1,8 +1,10 @@
 import os
 import time
-from dres_api import UserApi, ClientRunInfoApi, SubmissionApi, LogApi
+
+import dres_api
+from dres_api import UserApi, SubmissionApi
 from dres_api.configuration import Configuration
-from dres_api.models import LoginRequest, UserDetails, QueryResult, QueryEvent
+from dres_api.models import LoginRequest
 from dres_api.exceptions import ApiException
 from omegaconf import OmegaConf
 
@@ -10,25 +12,26 @@ from omegaconf import OmegaConf
 class Client:
     def __init__(self):
         config = OmegaConf.load(f'{ os.path.dirname(os.path.abspath(__file__))}/credentials.yaml')
+        self.configuration_instance = Configuration(**config)
+        self.session = 'node013jsjozq6rksbv6ua6iq8wy3c0'
 
-        # Setup
-        user_api = UserApi()
-        self.submission_api = SubmissionApi()
-        self.session_id = config.token
-        user_api.api_client.configuration.host = config.host
+        # Enter a context with an instance of the API client
+        with dres_api.ApiClient(self.configuration_instance) as api_client:
+            # Create an instance of the API class
+            api_instance = UserApi(api_client)
 
-        try:
-            login_request = LoginRequest(username=config.username, password=config.password)
-            login = user_api.post_api_v1_login(login_request)
-        except ApiException as ex:
-            print(f"Could not log in due to exception: {ex.message}")
-            return
+            try:
+                login_request = LoginRequest(**config)
+                login = api_instance.post_api_v1_login(login_request)
+            except ApiException as ex:
+                print(f"Could not log in due to exception: {ex.message}")
+                return
 
-        # Login successful
-        print(f"Successfully logged in.\n"
-              f"user: '{login.username}'\n"
-              f"role: '{login.role}'\n"
-              f"session_id: '{self.session_id}'")
+            # Login successful
+            print(login)
+            print(f"Successfully logged in.\n"
+                  f"user: '{login.username}'\n"
+                  f"role: '{login.role}'\n")
 
     def submit(self, item: str, frame: int, timecode: str,):
         """
@@ -39,28 +42,22 @@ class Client:
         :param timecode: Timecode for media with temporal progression (e.g. video).
         :return:
         """
-        submission_response = None
-        try:
-            submission_response = self.submission_api.get_api_v1_submit(
-                session=self.session_id,
-                collection=None,
-                item=item,
-                frame=frame,
-                shot=None,
-                timecode=timecode,
-                text=None
-            )
-        except ApiException as e:
-            if e.status == 401:
-                print("There was an authentication error during submission. Check the session id.")
-            elif e.status == 404:
-                print("There is currently no active task which would accept submissions.")
-            else:
-                print(f"Something unexpected went wrong during the submission: '{e.message}'.")
-                return
+        with dres_api.ApiClient(self.configuration_instance) as api_client:
+            # Create an instance of the API class
+            api_instance = SubmissionApi(api_client)
+            collection = 'None'
+            text = None
+            shot = frame
+            session = self.session
 
-        if submission_response is not None and submission_response.status:
-            print("The submission was successfully sent to the server.")
+            try:
+                # Endpoint to accept submissions
+                api_response = api_instance.get_api_v1_submit(collection=collection, item=item, text=text, frame=frame,
+                                                              shot=shot, timecode=timecode, session=session)
+                print("The response of SubmissionApi->get_api_v1_submit:\n")
+                print(api_response)
+            except Exception as e:
+                print("Exception when calling SubmissionApi->get_api_v1_submit: %s\n" % e)
 
 
 if __name__ == '__main__':
