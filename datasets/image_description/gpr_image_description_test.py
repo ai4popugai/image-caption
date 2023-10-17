0cf82cfc75d276969d743ac3e82e722617fa706c
ai4popugai <ivan@pheon.com>
2023-10-17 16:54:25 +0200

    adjust concept detection dataset test

--- datasets/image_description/gpr_image_description_test.py
+++ datasets/image_description/gpr_image_description_test.py
@@ -3,7 +3,6 @@ from datasets.image_description.gpr_image_description import GPRConceptsDataset
 
 
 def main():
-    os.environ['GPR_DATASET_CONCEPT_DETECTION'] = '/Users/iivan/datasets/gpr12000_concept_detection_v0'
     dataset = GPRConceptsDataset()
     item = dataset[1]
     print(item)
