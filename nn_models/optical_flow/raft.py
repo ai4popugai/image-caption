f5debe3902a358ebfb65f8d649d47329802a409e
ai4popugai <ivan@pheon.com>
2023-12-06 18:50:14 +0100

    adapt RAFT class to project

--- nn_models/optical_flow/raft.py
+++ nn_models/optical_flow/raft.py
@@ -1,11 +1,14 @@
-from typing import Union
+from typing import Dict
 
 import torch
-from torch import Tensor, nn
+from torch import nn
 from torchvision.models.optical_flow import raft_large, raft_small, Raft_Small_Weights, Raft_Large_Weights
 
+from datasets import FRAME_T, FRAME_T_K, OPTICAL_FLOW
+from nn_models.base_model import BaseModel
 
-class Raft(nn.Module):
+
+class Raft(BaseModel):
     def __init__(self, small: bool = False):
         super().__init__()
 
@@ -18,18 +21,11 @@ class Raft(nn.Module):
             self.transforms = weights.transforms()
             self.model = raft_large(weights=weights, progress=False)
 
-    def forward(self, x: Tensor, y: Tensor) -> Tensor:
+    def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         """
-        PTAl x and y must be non - normalized.
-        :param x: tensor on step t
-        :param y: tensor on step t + k
+        Batch must have keys for frame on timestamp t and frame on timestamp t_k.
+        PTAl t and t_k must be non - normalized.
         :return:
         """
-        x, y = self.transforms(x, y)
-        return self.model(x, y)
-
-    def to(self, device: Union[str, torch.device]):
-        self.model.to(device)
-
-    def eval(self):
-        self.model.eval()
+        t, t_k = self.transforms(batch[FRAME_T], batch[FRAME_T_K])
+        return {OPTICAL_FLOW: self.model(t, t_k)}
