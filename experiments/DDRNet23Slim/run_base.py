a96a48dbbd2989cd1a9cbf906050b907ed66bc78
ai4popugai <ivan@pheon.com>
2023-12-09 19:07:03 +0100

    add DDRNet23Slim RunBase stub

---
+++ experiments/DDRNet23Slim/run_base.py
@@ -0,0 +1,26 @@
+from torch import nn
+
+from datasets import FRAME_KEY
+from datasets.segmantation.cityscapes import CITYSCAPES_NUM_CLASSES
+from nn_models.segmentation.ddrnet.models import DDRNet23Slim
+from normalize.normalize import BatchNormalizer
+from train.run import Run
+from torchvision import transforms
+
+
+class RunBase(Run):
+    def __init__(self, filename: str):
+        super().__init__(filename)
+
+        self.num_classes = CITYSCAPES_NUM_CLASSES
+
+        self._normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        self.normalizer = BatchNormalizer(normalizer=self._normalizer, target_key=FRAME_KEY)
+
+        self.batch_size = 64
+        self.num_workers = 8
+
+    def setup_model(self) -> nn.Module:
+        return DDRNet23Slim(num_classes=self.num_classes)
+
+
