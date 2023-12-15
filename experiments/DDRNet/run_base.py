166c32c9ae44816a4754996da4ac6e17e4e97d2c
ai4popugai <ivan@pheon.com>
2023-12-15 16:25:02 +0100

    change augmentation in base DDRNet run

--- experiments/DDRNet/run_base.py
+++ experiments/DDRNet/run_base.py
@@ -42,12 +42,16 @@ class RunBase(Run):
         target_keys = [FRAME_KEY, GROUND_TRUTH_KEY]
         self.train_augs = [RandomFlip(target_keys=target_keys),
                            RandomCrop(self.crop_size, target_keys=target_keys),
-                           Rotate(angle_range=(-30, 30), target_keys=target_keys),
+                           # Rotate(angle_range=(-30, 30), target_keys=target_keys),
+                           # due to rotate edges' colors become different
                            RandomColorJitterWithProb(probability=0.8,
                                                      brightness_range=(0.7, 1),
                                                      contrast_range=(0.7, 1),
-                                                     saturation_range=(0.7, 1),
-                                                     hue_range=(0.3, 0.5))]
+                                                     # hue_range=(0.3, 0.5),
+                                                     # due to hue images change their original colors
+                                                     saturation_range=(0.7, 1)),
+
+                           ]
         self.val_augs = [CenterCrop(self.crop_size, target_keys=target_keys)]
 
         self.batch_dump_flag = True
