f967bf2331ea7c403497061d11fcb03dc17f7fe8
ai4popugai <ivan@pheon.com>
2023-12-23 16:32:41 +0100

    fix new run to use new crop_size in augs

--- experiments/DDRNet/run_0_find_lr_1/phase.py
+++ experiments/DDRNet/run_0_find_lr_1/phase.py
@@ -1,5 +1,6 @@
 import os
 
+from augmentations.augs import RandomFlip, RandomCrop, RandomColorJitterWithProb, CenterCrop
 from experiments.DDRNet.run_base import RunBase
 from optim_utils.iter_policy.linear_policy import LinearIterationPolicy
 
@@ -9,6 +10,18 @@ class Phase(RunBase):
         super().__init__(os.path.abspath(__file__))
 
         self.crop_size = (1024, 1024)
+        self.train_augs = [RandomFlip(target_keys=self.target_keys),
+                           RandomCrop(self.crop_size, target_keys=self.target_keys),
+                           # Rotate(angle_range=(-30, 30), target_keys=target_keys),
+                           # due to rotate edges' colors become different
+                           RandomColorJitterWithProb(probability=0.8,
+                                                     brightness_range=(0.7, 1),
+                                                     contrast_range=(0.7, 1),
+                                                     # hue_range=(0.3, 0.5),
+                                                     # due to hue images change their original colors
+                                                     saturation_range=(0.7, 1)),
+                           ]
+        self.val_augs = [CenterCrop(self.crop_size, target_keys=self.target_keys)]
 
         self.optimizer_kwargs = {'lr': 0., 'weight_decay': 3e-5}
         self.lr_policy = LinearIterationPolicy(start_iter=0, start_lr=0, end_lr=60000, end_val=9e-3)
