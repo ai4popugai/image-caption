de4521fb24318639944168a161cfc07a43c41c7b
ai4popugai <ivan@pheon.com>
2023-12-26 01:12:17 +0100

    add new run to find lr

---
+++ experiments/DDRNet/run_0_find_lr_4/phase_1.py
@@ -0,0 +1,16 @@
+import os
+
+from experiments.DDRNet.run_base import RunBase
+from optim_utils.iter_policy.linear_policy import LinearIterationPolicy
+
+
+class Phase(RunBase):
+    def __init__(self):
+        super().__init__(os.path.abspath(__file__))
+
+        self.optimizer_kwargs = {'lr': 0., 'weight_decay': 3e-5}
+        self.lr_policy = LinearIterationPolicy(start_iter=0, start_lr=0, end_lr=4000, end_val=8e-2)
+
+
+if __name__ == '__main__':
+    Phase().train(start_snapshot=None)
