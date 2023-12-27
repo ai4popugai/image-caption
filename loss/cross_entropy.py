87bf310a00c7867d5c6fdb3b8f2caad5101bb8b4
ai4popugai <ivan@pheon.com>
2023-12-27 19:24:07 +0100

    add ignore_index to CrossEntropyLoss

--- loss/cross_entropy.py
+++ loss/cross_entropy.py
@@ -1,11 +1,11 @@
-from typing import Dict
+from typing import Dict, Optional
 
 import torch
 from torch import nn
 
 
 class CrossEntropyLoss(nn.Module):
-    def __init__(self, result_trg_key: str, batch_trg_key: str):
+    def __init__(self, result_trg_key: str, batch_trg_key: str, ignore_index: int = -100):
         """
         Cross Entropy loss wrapper.
 
@@ -15,7 +15,7 @@ class CrossEntropyLoss(nn.Module):
         super().__init__()
         self.result_trg_key = result_trg_key
         self.batch_trg_key = batch_trg_key
-        self.loss = nn.CrossEntropyLoss()
+        self.loss = nn.CrossEntropyLoss(ignore_index=ignore_index)
 
     def forward(self, result: Dict[str, torch.Tensor], batch: Dict[str, torch.Tensor]) -> torch.Tensor:
         return self.loss(result[self.result_trg_key], batch[self.batch_trg_key].to(torch.int64))
