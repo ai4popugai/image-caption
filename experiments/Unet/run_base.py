f3dde81617fbd668fcd8d71456cd386d4cb33fa2
ai4popugai <ivan@pheon.com>
2024-01-04 22:08:06 +0100

    delete the most runs, adjust run_base for DDRNet and add run_base and first run for Unet (on Dubai Aerial)

--- experiments/Unet/run_base.py
+++ experiments/Unet/run_base.py
@@ -3,14 +3,14 @@ from typing import Tuple, Dict
 
 import torch
 from torch import nn
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, random_split
 
 from augmentations.augs import RandomFlip, RandomCrop, CenterCrop, RandomColorJitterWithProb, RandomResizedCropWithProb
 from datasets import FRAME_KEY, GROUND_TRUTH_KEY, LOGIT_KEY
-from datasets.segmantation.cityscapes import CityscapesDataset34
+from datasets.segmantation.dubai_aerial import DubaiAerial
 from loss.cross_entropy import CrossEntropyLoss
 from metrics.segmentation.iou import IoU
-from nn_models.segmentation.ddrnet.models import DDRNet23Slim
+from nn_models.segmentation.unet import Unet
 from normalize.normalize import BatchNormalizer
 from train.run import Run
 from torchvision import transforms
@@ -24,12 +24,12 @@ class RunBase(Run):
     def __init__(self, filename: str):
         super().__init__(filename)
 
-        self.num_classes = 19
+        self.num_classes = 6
 
         self._normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         self.normalizer = BatchNormalizer(normalizer=self._normalizer, target_key=FRAME_KEY)
 
-        self.batch_size = 8
+        self.batch_size = 16
         self.num_workers = 4
 
         self.train_iters = 500
@@ -37,15 +37,14 @@ class RunBase(Run):
         self.snapshot_iters = 500
         self.show_iters = 5
 
-        self.loss = CrossEntropyLoss(result_trg_key=LOGIT_KEY, batch_trg_key=GROUND_TRUTH_KEY,
-                                     ignore_index=19)
+        self.loss = CrossEntropyLoss(result_trg_key=LOGIT_KEY, batch_trg_key=GROUND_TRUTH_KEY)
 
         self.optimizer_class = torch.optim.Adam
 
         self.train_metrics = [IoU(self.num_classes)]
         self.val_metrics = [IoU(self.num_classes)]
 
-        self.crop_size = (1024, 1024)
+        self.crop_size = (256, 256)
 
         self.target_keys = [FRAME_KEY, GROUND_TRUTH_KEY]
 
@@ -64,14 +63,17 @@ class RunBase(Run):
         self.batch_dump_flag = True
 
     def setup_model(self) -> nn.Module:
-        return DDRNet23Slim(num_classes=self.num_classes)
+        return Unet(num_classes=self.num_classes)
 
     def setup_datasets(self) -> Tuple[Dataset, Dataset]:
-        train_dataset, val_dataset = CityscapesDataset34(mode='fine', split='train'), \
-                                     CityscapesDataset34(mode='fine', split='val')
+        torch.manual_seed(42)
+        dataset = DubaiAerial()
+        train_size = int(0.8 * len(dataset))
+        val_size = len(dataset) - train_size
+        train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
         return train_dataset, val_dataset
 
     def get_batch_sample_to_image_map(self) -> Dict[str, BaseToImageTransforms]:
         return {FRAME_KEY: FramesToImage(),
-                GROUND_TRUTH_KEY: GroundTruthToImage(color_map=CityscapesDataset34.color_map),
-                LOGIT_KEY: LogitsToImage(color_map=CityscapesDataset34.color_map)}
+                GROUND_TRUTH_KEY: GroundTruthToImage(color_map=DubaiAerial.color_map),
+                LOGIT_KEY: LogitsToImage(color_map=DubaiAerial.color_map)}
