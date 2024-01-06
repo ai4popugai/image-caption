687e804a03d357676da3b75eb06a918222a93e05
ai4popugai <ivan@pheon.com>
2024-01-06 18:27:02 +0100

    add reduction to the Focal loss

--- loss/focal_loss.py
+++ loss/focal_loss.py
@@ -7,7 +7,7 @@ from torchgeometry.losses import FocalLoss as FocalLossTorch
 
 class FocalLoss(nn.Module):
     def __init__(self, result_trg_key: str, batch_trg_key: str, alpha: float = 0.25,
-                 gamma: float = 2.):
+                 gamma: float = 2, reduction: str = 'mean'):
         """
         Cross Entropy loss wrapper.
 
@@ -17,7 +17,7 @@ class FocalLoss(nn.Module):
         super().__init__()
         self.result_trg_key = result_trg_key
         self.batch_trg_key = batch_trg_key
-        self.loss = FocalLossTorch(gamma=gamma, alpha=alpha)
+        self.loss = FocalLossTorch(gamma=gamma, alpha=alpha, reduction=reduction)
 
     def forward(self, result: Dict[str, torch.Tensor], batch: Dict[str, torch.Tensor]) -> torch.Tensor:
         return self.loss(result[self.result_trg_key], batch[self.batch_trg_key])
