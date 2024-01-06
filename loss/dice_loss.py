30062f55a7efcbd1c7e6043b7dcaa99577402220
ai4popugai <ivan@pheon.com>
2024-01-06 17:05:49 +0100

    add dice loss

---
+++ loss/dice_loss.py
@@ -0,0 +1,22 @@
+from typing import Dict
+
+import torch
+from torch import nn
+from torchgeometry.losses.dice import DiceLoss as DiceLossTorch
+
+
+class DiceLoss(nn.Module):
+    def __init__(self, result_trg_key: str, batch_trg_key: str):
+        """
+        Cross Entropy loss wrapper.
+
+        :param result_trg_key: key for the item from net's output to compute loss.
+        :param batch_trg_key: key for the item from batch to compute loss.
+        """
+        super().__init__()
+        self.result_trg_key = result_trg_key
+        self.batch_trg_key = batch_trg_key
+        self.loss = DiceLossTorch()
+
+    def forward(self, result: Dict[str, torch.Tensor], batch: Dict[str, torch.Tensor]) -> torch.Tensor:
+        return self.loss(result[self.result_trg_key], batch[self.batch_trg_key])
