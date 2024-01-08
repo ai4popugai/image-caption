replace LOGIT_AUG_KEY with LOGIT_AUX_KEY, add scaling to cross entropy, add ignore index to iou, add to base_run DDRNet cross entropy loss with aux, fix keyerror in batch_dump

experiments/DDRNet/run_base.py
@@ -6,7 +6,7 @@ from torch import nn
 from torch.utils.data import Dataset
 
 from augmentations.augs import RandomFlip, RandomCrop, CenterCrop, RandomColorJitterWithProb, RandomResizedCropWithProb
-from datasets import FRAME_KEY, GROUND_TRUTH_KEY, LOGIT_KEY
+from datasets import FRAME_KEY, GROUND_TRUTH_KEY, LOGIT_KEY, LOGIT_AUX_KEY
 from datasets.segmantation.cityscapes import CityscapesDataset19
 from loss.cross_entropy import CrossEntropyLoss
 from metrics.segmentation.iou import IoU
@@ -25,12 +25,13 @@ class RunBase(Run):
         super().__init__(filename)
 
         self.num_classes = 19
+        self.ignore_index = 19
 
         self._normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         self.normalizer = BatchNormalizer(normalizer=self._normalizer, target_key=FRAME_KEY)
 
         self.batch_size = 8
-        self.num_workers = 4
+        self.num_workers = 0
 
         self.train_iters = 500
         self.batch_dump_iters = 500
@@ -38,13 +39,17 @@ class RunBase(Run):
         self.show_iters = 5
         self.accum_iters = 4
 
-        self.loss = CrossEntropyLoss(result_trg_key=LOGIT_KEY, batch_trg_key=GROUND_TRUTH_KEY,
-                                     ignore_index=19)
+        self.alpha = 0.4
+        self.loss = [CrossEntropyLoss(result_trg_key=LOGIT_KEY, batch_trg_key=GROUND_TRUTH_KEY,
+                                      ignore_index=self.ignore_index),
+                     CrossEntropyLoss(result_trg_key=LOGIT_AUX_KEY, batch_trg_key=GROUND_TRUTH_KEY,
+                                      ignore_index=self.ignore_index, alpha=self.alpha)
+                     ]
 
         self.optimizer_class = torch.optim.Adam
 
-        self.train_metrics = [IoU(self.num_classes)]
-        self.val_metrics = [IoU(self.num_classes)]
+        self.train_metrics = [IoU(self.num_classes, ignore_index=self.ignore_index)]
+        self.val_metrics = [IoU(self.num_classes, ignore_index=self.ignore_index)]
 
         self.crop_size = (1024, 1024)
 
@@ -65,7 +70,7 @@ class RunBase(Run):
         self.batch_dump_flag = True
 
     def setup_model(self) -> nn.Module:
-        return DDRNet23Slim(num_classes=self.num_classes)
+        return DDRNet23Slim(num_classes=self.num_classes, aux=True)
 
     def setup_datasets(self) -> Tuple[Dataset, Dataset]:
         train_dataset, val_dataset = CityscapesDataset19(mode='fine', split='train'), \
@@ -75,4 +80,5 @@ class RunBase(Run):
     def get_batch_sample_to_image_map(self) -> Dict[str, BaseToImageTransforms]:
         return {FRAME_KEY: FramesToImage(),
                 GROUND_TRUTH_KEY: GroundTruthToImage(color_map=CityscapesDataset19.color_map),
-                LOGIT_KEY: LogitsToImage(color_map=CityscapesDataset19.color_map)}
+                LOGIT_KEY: LogitsToImage(color_map=CityscapesDataset19.color_map),
+                LOGIT_AUX_KEY: LogitsToImage(color_map=CityscapesDataset19.color_map)}

loss/cross_entropy.py
@@ -5,7 +5,7 @@ from torch import nn
 
 
 class CrossEntropyLoss(nn.Module):
-    def __init__(self, result_trg_key: str, batch_trg_key: str, ignore_index: int = -100):
+    def __init__(self, result_trg_key: str, batch_trg_key: str, ignore_index: int = -100, alpha: float = 1.):
         """
         Cross Entropy loss wrapper.
 
@@ -16,6 +16,8 @@ class CrossEntropyLoss(nn.Module):
         self.result_trg_key = result_trg_key
         self.batch_trg_key = batch_trg_key
         self.loss = nn.CrossEntropyLoss(ignore_index=ignore_index)
+        self.alpha = torch.tensor(alpha)
 
     def forward(self, result: Dict[str, torch.Tensor], batch: Dict[str, torch.Tensor]) -> torch.Tensor:
-        return self.loss(result[self.result_trg_key], batch[self.batch_trg_key])
+        return self.loss(result[self.result_trg_key], batch[self.batch_trg_key]) * \
+               self.alpha.to(batch[self.batch_trg_key].device)

nn_models/segmentation/ddrnet/models.py
@@ -3,7 +3,7 @@ from typing import Dict, Optional
 import torch
 from torch import nn
 
-from datasets import LOGIT_KEY, FRAME_KEY, LOGIT_AUG_KEY
+from datasets import LOGIT_KEY, FRAME_KEY, LOGIT_AUX_KEY
 from .utils import DualResNet, BasicBlock
 
 
@@ -16,7 +16,7 @@ class BaseDDRNet(nn.Module):
     def forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         model_out = self.model(batch[FRAME_KEY])
         return {LOGIT_KEY: model_out} if self.aux is False else {LOGIT_KEY: model_out[0],
-                                                                 LOGIT_AUG_KEY: model_out[1]}
+                                                                 LOGIT_AUX_KEY: model_out[1]}
 
 
 class DDRNet23Slim(BaseDDRNet):

train/train.py
@@ -228,9 +228,10 @@ class Trainer:
     def _batch_dump(self, batch: Dict[str, torch.Tensor], iteration: int, mode: str,):
         if iteration % self.batch_dump_iters == 0 and self.batch_dump_flag:
             for key in batch:
-                imgs = self.sample_to_image[key](batch[key])
-                for i, img in enumerate(imgs):
-                    cv2.imwrite(os.path.join(self.batch_dump_dir, f'{mode}_iter_{iteration}__{i}_{key}.png'), img)
+                if key in self.sample_to_image.keys():
+                    imgs = self.sample_to_image[key](batch[key])
+                    for i, img in enumerate(imgs):
+                        cv2.imwrite(os.path.join(self.batch_dump_dir, f'{mode}_iter_{iteration}__{i}_{key}.png'), img)
 
     def _train_iteration(self, model: nn.Module, batch: Dict[str, torch.Tensor], iteration: int) -> float:
         model.train()