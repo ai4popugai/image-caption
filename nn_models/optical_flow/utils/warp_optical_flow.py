006cf30ed39fb965d9fbd70a70608eab48210741
ai4popugai <ivan@pheon.com>
2024-01-31 09:16:52 +0100

    add method to warp image with optical flow

---
+++ nn_models/optical_flow/utils/warp_optical_flow.py
@@ -0,0 +1,23 @@
+import torch
+
+
+def warp_optical_flow(img: torch.Tensor, flow: torch.Tensor) -> torch.Tensor:
+    device = f'cuda:{img.get_device()}' if img.get_device() >= 0 else 'cpu'
+
+    if img.shape[0] != flow.shape[0]:
+        raise RuntimeError('Two input batches has different batch sizes.')
+    if img.shape[-2] != flow.shape[-2]:
+        raise RuntimeError('Two input batches has different heights.')
+    if img.shape[-1] != flow.shape[-1]:
+        raise RuntimeError('Two input batches has different widths.')
+
+    h, w = img.shape[-2:]
+
+    grid_y, grid_x = torch.meshgrid(torch.arange(h), torch.arange(w))
+    grid = torch.stack((grid_x, grid_y), dim=-1).float().to(device)
+    grid = grid.unsqueeze(dim=0).repeat(img.shape[0], 1, 1, 1)
+    flow = flow.permute(0, 2, 3, 1)
+    warped_grid = grid - flow
+    warped_grid = warped_grid * 2 / torch.tensor([w, h]).float().to(device) - 1
+    warped_image = torch.nn.functional.grid_sample(img.float(), warped_grid, padding_mode='zeros')
+    return warped_image
